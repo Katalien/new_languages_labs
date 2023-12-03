5fc55acc9c078b5a76c4f4ac3f9b7d65a62e35fa
Katalien <90635745+Katalien@users.noreply.github.com>
2023-12-03 17:36:26 +0300

    Update palindrom.swift

--- Swift/palindrom.swift
+++ Swift/palindrom.swift
@@ -1,59 +1,58 @@
-import Swift
 import Foundation
 
-func isPalindrome(_ s: String) -> Bool {
-    if s.count < 2 {
+func isPalindrome(_ s: String, _ startIdx: String.Index, _ endIdx: String.Index) -> Bool {
+    if startIdx >= endIdx {
         return true
     } else {
-        if s.first! == s.last! {
-            let startIndex = s.index(after: s.startIndex)
-            let endIndex = s.index(before: s.endIndex)
-            return isPalindrome(String(s[startIndex..<endIndex]))
+        if s[startIdx] == s[endIdx] {
+            let nextStartIdx = s.index(after: startIdx)
+            let prevEndIdx = s.index(before: endIdx)
+            return isPalindrome(s, nextStartIdx, prevEndIdx)
         } else {
             return false
         }
     }
 }
 
-print("You're going to check if your word is a palindrom.\n")
-print("You can enter a word from console or read it from file.\n Type 'c' for conslole and 'f' for file")
-let choice = readLine()
-if choice == "c"{
-    while true {
-        print("Enter a string or click Enter to finish:")
-        if let input = readLine() {
-            if input.isEmpty {
-                break
-            }
-            let cleanedInput = input.lowercased().filter { $0.isLetter }
-            if isPalindrome(cleanedInput) {
-                print("\(input) is a palindrom!\n")
-            } else {
-                print("\(input) is NOT a palindrom!\n")
+print("You're going to check if your word is a palindrome.\n")
+print("You can enter a word from console or read it from a file.\nType 'c' for console and 'f' for file")
+if let choice = readLine() {
+    if choice == "c" {
+        while true {
+            print("Enter a string or click Enter to finish:")
+            if let input = readLine() {
+                if input.isEmpty {
+                    break
+                }
+                let cleanedInput = input.lowercased().filter { $0.isLetter }
+                let startIndex = cleanedInput.startIndex
+                let endIndex = cleanedInput.index(before: cleanedInput.endIndex)
+                if isPalindrome(cleanedInput, startIndex, endIndex) {
+                    print("\(input) is a palindrome!\n")
+                } else {
+                    print("\(input) is NOT a palindrome!\n")
+                }
             }
         }
-    }
-}
-if choice == "f"{
-    print("Enter a filename (with extension)")
-    if let fileName = readLine() {
-        if !fileName.isEmpty {
-           do {
+    } else if choice == "f" {
+        print("Enter a filename (with extension)")
+        if let fileName = readLine(), !fileName.isEmpty {
+            do {
                 let content = try String(contentsOfFile: fileName, encoding: .utf8)
-                let fileContent = content
-            
-                if isPalindrome(fileContent) {
-                    print("\(fileContent) is a palindrom!")
-                }
-                else{
-                    print("\(fileContent) is NOT a palindrom!")
+                let fileContent = content.lowercased().filter { $0.isLetter }
+                let startIndex = fileContent.startIndex
+                let endIndex = fileContent.index(before: fileContent.endIndex)
+                if isPalindrome(fileContent, startIndex, endIndex) {
+                    print("\(fileContent) is a palindrome!")
+                } else {
+                    print("\(fileContent) is NOT a palindrome!")
                 }
             } catch {
                 print("Error while reading a file:\n", error)
             }
         }
     }
-    
 }
 print("Goodbye!")
+
 
